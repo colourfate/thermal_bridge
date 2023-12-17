382f389b279db5bd0b927dd416bc012663524c5e
colourfate <chdlogitech@163.com>
2023-12-17 15:54:01 +0800

    Change usb packet format
    Signed-off-by: colourfate <chdlogitech@163.com>

--- Core/Src/usb_data_transfer.c
+++ Core/Src/usb_data_transfer.c
@@ -8,30 +8,42 @@
 #include "common.h"
 
 #define DATA_EOF_LEN 2
+#define USB_DATA_HEAD 0x8000
+
+enum {
+	USB_DATA_TYPE_CMD_FPS,
+	USB_DATA_TYPE_DATA,
+};
+
+typedef struct {
+	uint16_t header;
+	uint16_t type;
+	uint16_t len;
+	uint8_t data[0];
+} usb_data_format;
 
 static uint8_t g_usb_tx_buffer[APP_TX_DATA_SIZE];
-static uint32_t g_data_len;
+static usb_data_format *g_packet = (usb_data_format *)g_usb_tx_buffer;
+
 
 int usb_data_store_f(float *data, uint32_t data_len)
 {
-	uint32_t packet_len = data_len * 2 + DATA_EOF_LEN;
+	uint32_t packet_len = data_len * 2 + sizeof(usb_data_format);
 	uint32_t i;
 
 	if (packet_len > count_of(g_usb_tx_buffer)) {
 		perr("Data len %ld must < %d\r\n", packet_len, count_of(g_usb_tx_buffer));
 		return -1;
 	}
-	g_data_len = packet_len;
 
-	memset(g_usb_tx_buffer, 0, g_data_len);
+	memset(g_usb_tx_buffer, 0, count_of(g_usb_tx_buffer));
+	g_packet->header = USB_DATA_HEAD;
+	g_packet->type = USB_DATA_TYPE_DATA;
+	g_packet->len = data_len * 2;
 	for (i = 0; i < data_len; i++) {
-		int16_t tmp = (int16_t)(data[i] * 100);
-
-		g_usb_tx_buffer[2 * i] = (uint8_t)((uint16_t)tmp >> 8);
-		g_usb_tx_buffer[2 * i + 1] = (uint8_t)tmp;
+		*((uint16_t *)(&g_packet->data[2 * i])) = (uint16_t)(data[i] * 100);
 	}
-	g_usb_tx_buffer[2 * i] = 0x80;
-	g_usb_tx_buffer[2 * i + 1] = 0;
+
 
 	return 0;
 }
@@ -41,6 +53,6 @@ void usb_data_send(void)
 	uint32_t start;
 
 	start = HAL_GetTick();
-	CDC_Transmit_FS(g_usb_tx_buffer, g_data_len);
+	CDC_Transmit_FS((uint8_t *)g_packet, g_packet->len + sizeof(usb_data_format));
 	pinfo("send data: %ld\r\n", HAL_GetTick() - start);
 }
